e203f64a429349a9c36731086a5a6355ff19f279
Sikay <pedrohdezmora@gmail.com>
2019-09-26 19:25:36 +0200

    wip

--- Tests/Src/MoneyTest.php
+++ Tests/Src/MoneyTest.php
@@ -3,8 +3,141 @@
 namespace WeDev\Price\Tests\Src;
 
 use PHPUnit\Framework\TestCase;
+use WeDev\Price\Domain\Currency;
+use WeDev\Price\Domain\Number;
+use WeDev\Price\Domain\Money;
+
 
 class MoneyTest extends TestCase
 {
+    private const A_POSITIVE_DECIMAL_NUMBER = 1.5;
+    private const A_USD_ISO_CODE = 'USD';
 
+    /**
+     * @test
+     */
+    public function shouldNotBeEqualIfMoneyIsIncreased()
+    {
+        $number_a = Number::fromNumber(self::A_POSITIVE_DECIMAL_NUMBER);
+        $currency_a = Currency::fromIsoCode(self::A_USD_ISO_CODE);
+        $money_a = new Money($number_a, $currency_a);
+        $money_b = $money_a->increaseAmountBy(self::A_POSITIVE_DECIMAL_NUMBER);
+        $this->assertFalse($money_b->equals($money_a));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldBeEqualIfIncreaseBothMoney()
+    {
+        $number_a = Number::fromNumber(self::A_POSITIVE_DECIMAL_NUMBER);
+        $currency_a = Currency::fromIsoCode(self::A_USD_ISO_CODE);
+        $money_a = new Money($number_a, $currency_a);
+        $money_b = $money_a->increaseAmountBy(self::A_POSITIVE_DECIMAL_NUMBER);
+        $number_b = $number_a->add(self::A_POSITIVE_DECIMAL_NUMBER);
+        $money_c = new Money($number_b, $currency_a);
+        $this->assertTrue($money_b->equals($money_c));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldNotBeEqualIfMoneyIsDecrease()
+    {
+        $number_a = Number::fromNumber(self::A_POSITIVE_DECIMAL_NUMBER);
+        $currency_a = Currency::fromIsoCode(self::A_USD_ISO_CODE);
+        $money_a = new Money($number_a, $currency_a);
+        $money_b = $money_a->decreaseAmountBy(self::A_POSITIVE_DECIMAL_NUMBER);
+        $this->assertFalse($money_b->equals($money_a));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldBeEqualIfDecreaseBothMoney()
+    {
+        $number_a = Number::fromNumber(self::A_POSITIVE_DECIMAL_NUMBER);
+        $currency_a = Currency::fromIsoCode(self::A_USD_ISO_CODE);
+        $money_a = new Money($number_a, $currency_a);
+        $money_b = $money_a->decreaseAmountBy(self::A_POSITIVE_DECIMAL_NUMBER);
+        $number_b = $number_a->substract(self::A_POSITIVE_DECIMAL_NUMBER);
+        $money_c = new Money($number_b, $currency_a);
+        $this->assertTrue($money_b->equals($money_c));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldNotBeEqualIfMoneyIsMultiply()
+    {
+        $number_a = Number::fromNumber(self::A_POSITIVE_DECIMAL_NUMBER);
+        $currency_a = Currency::fromIsoCode(self::A_USD_ISO_CODE);
+        $money_a = new Money($number_a, $currency_a);
+        $money_b = $money_a->multiplyAmountBy(self::A_POSITIVE_DECIMAL_NUMBER);
+        $this->assertFalse($money_b->equals($money_a));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldBeEqualIfMultiplyBothMoney()
+    {
+        $number_a = Number::fromNumber(self::A_POSITIVE_DECIMAL_NUMBER);
+        $currency_a = Currency::fromIsoCode(self::A_USD_ISO_CODE);
+        $money_a = new Money($number_a, $currency_a);
+        $money_b = $money_a->multiplyAmountBy(self::A_POSITIVE_DECIMAL_NUMBER);
+        $number_b = $number_a->multiply(self::A_POSITIVE_DECIMAL_NUMBER);
+        $money_c = new Money($number_b, $currency_a);
+        $this->assertTrue($money_b->equals($money_c));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldNotBeEqualIfMoneyIsDivide()
+    {
+        $number_a = Number::fromNumber(self::A_POSITIVE_DECIMAL_NUMBER);
+        $currency_a = Currency::fromIsoCode(self::A_USD_ISO_CODE);
+        $money_a = new Money($number_a, $currency_a);
+        $money_b = $money_a->divideAmountBy(self::A_POSITIVE_DECIMAL_NUMBER);
+        $this->assertFalse($money_b->equals($money_a));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldBeEqualIfDivideBothMoney()
+    {
+        $number_a = Number::fromNumber(self::A_POSITIVE_DECIMAL_NUMBER);
+        $currency_a = Currency::fromIsoCode(self::A_USD_ISO_CODE);
+        $money_a = new Money($number_a, $currency_a);
+        $money_b = $money_a->divideAmountBy(self::A_POSITIVE_DECIMAL_NUMBER);
+        $number_b = $number_a->divide(self::A_POSITIVE_DECIMAL_NUMBER);
+        $money_c = new Money($number_b, $currency_a);
+        $this->assertTrue($money_b->equals($money_c));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldBeEqualBothMoney()
+    {
+        $number_a = Number::fromNumber(self::A_POSITIVE_DECIMAL_NUMBER);
+        $currency_a = Currency::fromIsoCode(self::A_USD_ISO_CODE);
+        $money_a = new Money($number_a, $currency_a);
+        $money_b = new Money($number_a, $currency_a);
+        $this->assertTrue($money_b->equals($money_a));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldNotBeTheSameMoney()
+    {
+        $number_a = Number::fromNumber(self::A_POSITIVE_DECIMAL_NUMBER);
+        $currency_a = Currency::fromIsoCode(self::A_USD_ISO_CODE);
+        $money_a = new Money($number_a, $currency_a);
+        $money_b = new Money($number_a, $currency_a);
+        $this->assertFalse($money_b === $money_a);
+    }
 }
